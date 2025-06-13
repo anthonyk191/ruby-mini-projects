c32ffd9ee7534c7daf81608e326edb099d3798b3
anthonyk191 <67760299+anthonyk191@users.noreply.github.com>
2025-06-13 02:22:55 -0700

    Cleaned up sub-string project, deleted redundant lines, made inputs and outputs similar to problem posted on website

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,33 +1,35 @@
-dictionary = {}
-dictionary_keys = ["how", "ow", "owh"]
-
-#Creating dictionary
-for i in dictionary_keys
-  dictionary[i] = 0
-end
+dictionary_keys = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 #Function
-def substrings(input, dictionary)
-    split_input = input.split("")
-
-
-    for i in dictionary.keys
-      dictionary_word = i
-      length_dictionary_word = dictionary_word.length
-      #Window Method
-      index_1 = 0
-      index_2 = length_dictionary_word - 1
-
-      for j in (0..split_input.length - length_dictionary_word) #Window slides through input string
-        if (split_input[index_1..index_2].join == dictionary_word) #Compares dictionary key to current window
-          dictionary[dictionary_word] += 1
-        end
-        index_1 += 1
-        index_2 += 1
+def substrings(input, dictionary_keys)
+  #Convert to Dictionary
+  dictionary = {}
+  for i in dictionary_keys
+    dictionary[i] = 0
+  end
+  
+  split_input = input.split("")
+  for key in dictionary.keys
+    #Window Method
+    index_1 = 0
+    index_2 = key.length - 1
+    
+    for j in (0..split_input.length - key.length) #Window slides through input string
+      if (split_input[index_1..index_2].join == key) #Compares dictionary key to current window
+        dictionary[key] += 1
       end
+      index_1 += 1
+      index_2 += 1
     end
-    p dictionary
+  end
+  
+  for key in dictionary.keys
+    if dictionary[key] == 0 #Value
+      dictionary.delete(key)
+    end
+  end
+  p dictionary
 end
 
-substrings("howhow", dictionary) #{"how" => 1}
+substrings("below", dictionary_keys)
 
